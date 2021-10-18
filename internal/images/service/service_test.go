5085610dd3399d24fb9c3dda814dab9b07a0b1bc
Michael Habib <michael.habib@couchbase.com>
2021-10-17 23:29:30 -0500

    gofmt

--- internal/images/service/service_test.go
+++ internal/images/service/service_test.go
@@ -2,11 +2,11 @@ package service
 
 import (
 	"errors"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"strings"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/golang/mock/gomock"
