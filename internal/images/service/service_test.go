13498517ba86bf8f6d62e424962e1a1eca966918
Michael Habib <michael.habib@couchbase.com>
2021-10-19 21:41:01 -0500

    wrap up unit tests for now, added for delete and download

--- internal/images/service/service_test.go
+++ internal/images/service/service_test.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"errors"
+	"io"
 	"strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -19,6 +20,234 @@ import (
 	internalS3 "github.com/itsHabib/sim/internal/s3"
 	mock_s3 "github.com/itsHabib/sim/internal/s3/mocks"
 )
+
+func Test_Service_Delete(t *testing.T) {
+	id := "id"
+	storage := "storage"
+	for _, tc := range []struct {
+		desc    string
+		reader  func(ctrl *gomock.Controller) images.Reader
+		writer  func(ctrl *gomock.Controller) images.Writer
+		client  func(ctrl *gomock.Controller) internalS3.Client
+		wantErr bool
+	}{
+		{
+			desc: "Delete() should return an error when failing to retrieve the object.",
+			reader: func(ctrl *gomock.Controller) images.Reader {
+				r := mock_images.NewMockReader(ctrl)
+				r.
+					EXPECT().
+					Get(id).
+					Return(nil, errors.New("random"))
+
+				return r
+			},
+			writer:  func(ctrl *gomock.Controller) images.Writer { return mock_images.NewMockWriter(ctrl) },
+			client:  func(ctrl *gomock.Controller) internalS3.Client { return mock_s3.NewMockClient(ctrl) },
+			wantErr: true,
+		},
+		{
+			desc: "Delete() should return an error when failing to delete the object in cloud storage.",
+			reader: func(ctrl *gomock.Controller) images.Reader {
+				r := mock_images.NewMockReader(ctrl)
+				r.
+					EXPECT().
+					Get(id).
+					Return(&images.Record{Key: "key"}, nil)
+
+				return r
+			},
+			writer: func(ctrl *gomock.Controller) images.Writer { return mock_images.NewMockWriter(ctrl) },
+			client: func(ctrl *gomock.Controller) internalS3.Client {
+				c := mock_s3.NewMockClient(ctrl)
+				c.
+					EXPECT().
+					DeleteObject(gomock.Any()).
+					Return(nil, errors.New("random"))
+
+				return c
+			},
+			wantErr: true,
+		},
+		{
+			desc: "Delete() should return an error when failing to delete the object in the DB.",
+			reader: func(ctrl *gomock.Controller) images.Reader {
+				r := mock_images.NewMockReader(ctrl)
+				r.
+					EXPECT().
+					Get(id).
+					Return(&images.Record{Key: "key"}, nil)
+
+				return r
+			},
+			writer: func(ctrl *gomock.Controller) images.Writer {
+				w := mock_images.NewMockWriter(ctrl)
+				w.
+					EXPECT().
+					Delete(id).
+					Return(errors.New("random"))
+
+				return w
+			},
+			client: func(ctrl *gomock.Controller) internalS3.Client {
+				c := mock_s3.NewMockClient(ctrl)
+				c.
+					EXPECT().
+					DeleteObject(gomock.Any()).
+					Return(nil, nil)
+
+				return c
+			},
+			wantErr: true,
+		},
+		{
+			desc: "Delete() - happy path",
+			reader: func(ctrl *gomock.Controller) images.Reader {
+				r := mock_images.NewMockReader(ctrl)
+				r.
+					EXPECT().
+					Get(id).
+					Return(&images.Record{Key: "key"}, nil)
+
+				return r
+			},
+			writer: func(ctrl *gomock.Controller) images.Writer {
+				w := mock_images.NewMockWriter(ctrl)
+				w.
+					EXPECT().
+					Delete(id).
+					Return(nil)
+
+				return w
+			},
+			client: func(ctrl *gomock.Controller) internalS3.Client {
+				c := mock_s3.NewMockClient(ctrl)
+				c.
+					EXPECT().
+					DeleteObject(gomock.Any()).
+					Return(nil, nil)
+
+				return c
+			},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			svc, err := New(zap.NewNop(), storage, tc.reader(ctrl), tc.writer(ctrl), mockSessionGetter)
+			svc.sdk.client = tc.client(ctrl)
+			require.NoError(t, err)
+
+			err = svc.Delete(id)
+			if tc.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func Test_Service_Download(t *testing.T) {
+	id := "id"
+	storage := "storage"
+	req := images.DownloadRequest{
+		ID: "id",
+	}
+	for _, tc := range []struct {
+		desc       string
+		reader     func(ctrl *gomock.Controller) images.Reader
+		downloader func(t *testing.T, ctrl *gomock.Controller) internalS3.Downloader
+		wantErr    bool
+	}{
+		{
+			desc: "Download() should return an error when failing to retrieve the image record.",
+			reader: func(ctrl *gomock.Controller) images.Reader {
+				r := mock_images.NewMockReader(ctrl)
+				r.
+					EXPECT().
+					Get(id).
+					Return(nil, errors.New("random"))
+
+				return r
+			},
+			wantErr: true,
+		},
+		{
+			desc: "Download() should return an error when failing to download the object.",
+			reader: func(ctrl *gomock.Controller) images.Reader {
+				r := mock_images.NewMockReader(ctrl)
+				r.
+					EXPECT().
+					Get(id).
+					Return(&images.Record{Key: "key"}, nil)
+
+				return r
+			},
+			downloader: func(t *testing.T, ctrl *gomock.Controller) internalS3.Downloader {
+				c := mock_s3.NewMockDownloader(ctrl)
+				c.
+					EXPECT().
+					Download(gomock.Any(), gomock.Any()).
+					Return(int64(0), errors.New("random"))
+
+				return c
+			},
+			wantErr: true,
+		},
+		{
+			desc: "Download() - happy path",
+			reader: func(ctrl *gomock.Controller) images.Reader {
+				r := mock_images.NewMockReader(ctrl)
+				r.
+					EXPECT().
+					Get(id).
+					Return(&images.Record{Key: "key"}, nil)
+
+				return r
+			},
+			downloader: func(t *testing.T, ctrl *gomock.Controller) internalS3.Downloader {
+				c := mock_s3.NewMockDownloader(ctrl)
+				c.
+					EXPECT().
+					Download(gomock.Any(), gomock.Any()).
+					DoAndReturn(func(_ io.WriterAt, i *s3.GetObjectInput, _ ...func(*s3manager.Downloader)) (int64, error) {
+						require.NotNil(t, i)
+						assert.Equal(t, "key", unwrapStr(i.Key))
+						assert.Equal(t, storage, unwrapStr(i.Bucket))
+
+						return 10, nil
+					})
+
+				return c
+			},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			r := mock_images.NewMockReader(ctrl)
+			if tc.reader == nil {
+				tc.reader = func(ctrl *gomock.Controller) images.Reader { return r }
+			}
+			d := mock_s3.NewMockDownloader(ctrl)
+			if tc.downloader == nil {
+				tc.downloader = func(_ *testing.T, ctrl *gomock.Controller) internalS3.Downloader { return d }
+			}
+
+			svc, err := New(zap.NewNop(), storage, tc.reader(ctrl), mock_images.NewMockWriter(ctrl), mockSessionGetter)
+			svc.sdk.downloader = tc.downloader(t, ctrl)
+			require.NoError(t, err)
+
+			err = svc.Download(req)
+			if tc.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
 
 func Test_Service_Upload(t *testing.T) {
 	storage := "sim"
